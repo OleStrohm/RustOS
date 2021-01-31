80d64b3d45f6d2dba6a6deef4d69487005f30fa9
OleStrohm <strohm99@gmail.com>
2021-01-31 15:21:08 +0000

    println! macro for vga printing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+mod vga;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -8,18 +10,10 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello world";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xB8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xB;
-        }
-    }
+    let name = "Ole";
+    println!("Hello, {}!", name);
 
     loop {}
 }
